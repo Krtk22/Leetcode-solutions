4590d022f6517f76144d4eef4deede7cf1ee1763
Krtk22 <kushwahakartik43@gmail.com>
2026-07-27 15:56:54 +0530

    Time: 23 ms (77.49%), Space: 31.5 MB (59.62%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,45 +1,21 @@
-// class Solution {
-//     // RECURSIVE APPROACH
-// public:
-//     int solve(string s, int i, int j) {
-//         // base case
-//         if(i >= j) return 0;
-
-//         if(s[i] == s[j]) {
-//             return 0 + solve(s, i + 1, j - 1);
-//         }
-
-//         int ith_ele = solve(s,i + 1, j);
-//         int jth_ele = solve(s,i, j - 1);
-
-//         return 1 + min(ith_ele, jth_ele);
-//     }
-
-//     int minInsertions(string s) {
-//         int n = s.size();
-//         return solve(s, 0, n - 1);
-//     }
-// };
-
 class Solution {
-    // MEMOIZATION APPROACH
+    // TABULATION APPROACH
 public:
-    int solve(string& s, int i, int j, vector<vector<int>>& dp) {
-        // base case
-        if(i >= j) return 0;
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        if(s[i] == s[j]) {
-            return dp[i][j] = 0 + solve(s, i + 1, j - 1, dp);
-        }
-
-        return dp[i][j] = 1 + min(solve(s,i + 1, j, dp), solve(s,i, j - 1, dp));
-    }
-
     int minInsertions(string s) {
         int n = s.size();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-        return solve(s, 0, n - 1, dp);
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        
+        // gap = length of substring - 1
+        for (int gap = 1; gap < n; gap++) {
+            for (int i = 0, j = gap; j < n; i++, j++) {
+                if (s[i] == s[j]) {
+                    dp[i][j] = dp[i+1][j-1];
+                } else {
+                    dp[i][j] = 1 + min(dp[i+1][j], dp[i][j-1]);
+                }
+            }
+        }
+
+        return dp[0][n-1];
     }
 };
