7b8f3f61bd69dbd8de7997474a6806ebdafab693
Krtk22 <kushwahakartik43@gmail.com>
2026-07-30 11:40:12 +0530

    Time: 0 ms (100%), Space: 8.3 MB (77.73%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,17 +1,12 @@
 class Solution {
+    // OPTIMIZE
 public:
     int minimumPushes(string word) {
         int n = word.size();
-        if(n <= 8) return n;
-        
-        int push = 0;
-        for(int i = 0; i < n; i++) {
-            if(i <= 7) push += 1;
-            if(i > 7 && i <= 15) push += 2;
-            if(i > 15 && i <= 23) push += 3;
-            if(i > 23) push += 4;
-        }
+        if (n <= 8) return n;
+        if (n <= 16) return 8 + (n - 8) * 2;
+        if (n <= 24) return 8 + 16 + (n - 16) * 3;
+        return 8 + 16 + 24 + (n - 24) * 4;
 
-        return push;
     }
 };
