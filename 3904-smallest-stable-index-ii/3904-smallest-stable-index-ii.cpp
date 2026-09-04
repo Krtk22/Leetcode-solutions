9e34b869ad13225039c010781c5ee71df1b9e94c
Krtk22 <kushwahakartik43@gmail.com>
2026-09-04 21:51:14 +0530

    Time: 27 ms (9.46%), Space: 210.9 MB (35.82%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -3,21 +3,27 @@ public:
     int firstStableIndex(vector<int>& nums, int k) {
         int n = nums.size();
 
-        vector<int> minFromIndex(n);
-        int minEl = INT_MAX;
-        for(int i = n-1; i >= 0; i--) {
-            minEl = min(minEl, nums[i]);
-            minFromIndex[i] = minEl;
-        }
+        // using prefix array concept
+        vector<int> maxEle(n, 0);
+        vector<int> minEle(n, 0);
 
-        int maxEl = 0;
+        // filling max element array
+        maxEle[0] = nums[0];
+        for(int i = 1; i < n; i++) {
+            maxEle[i] = max(maxEle[i - 1], nums[i]);
+        }
+        // filling min element
+        minEle[n - 1] = nums[n - 1];
+        for(int i = n - 2; i >= 0; i--) {
+            minEle[i] = min(minEle[i + 1], nums[i]);
+        }
+        int result = INT_MAX;
         for(int i = 0; i < n; i++) {
-            maxEl = max(maxEl, nums[i]);
-
-            if(maxEl - minFromIndex[i] <= k)
-                return i;
+            int temp = maxEle[i] - minEle[i];
+            if(temp <= k) {
+                result = min(result, i);
+            }
         }
-        
-        return -1;
+        return (result == INT_MAX) ? -1 : result;
     }
 };
