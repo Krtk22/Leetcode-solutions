1aecc36d89b533e247de2d0b84d0fce62469be9a
Krtk22 <kushwahakartik43@gmail.com>
2026-09-07 12:07:55 +0530

    Time: 30 ms (58.76%), Space: 44.2 MB (38.57%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,33 +1,53 @@
 class Solution {
-    // MEMOIZATION
 public:
     typedef unsigned long long ull;
 
-    ull dp[1001][1001];
+    // MEMOIZATION
+    // ull dp[1001][1001];
 
-    ull solve(string s, string t, int m, int n) {
+    // ull solve(string s, string t, int m, int n) {
         
-        if(n == 0) return dp[m][n] = 1; // found 1 sub sequence
-        if(m == 0) return dp[m][n] = 0; // s string exhausted
+    //     if(n == 0) return dp[m][n] = 1; // found 1 sub sequence
+    //     if(m == 0) return dp[m][n] = 0; // s string exhausted
 
-        if(dp[m][n] != -1) return dp[m][n];
+    //     if(dp[m][n] != -1) return dp[m][n];
 
-        if(s[m - 1] == t[n - 1]) {
-            return dp[m][n] = solve(s, t, m - 1, n - 1) + solve(s, t, m - 1, n);
-        } else {
-            return dp[m][n] = solve(s, t, m - 1, n);
-        }
+    //     if(s[m - 1] == t[n - 1]) {
+    //         return dp[m][n] = solve(s, t, m - 1, n - 1) + solve(s, t, m - 1, n);
+    //     } else {
+    //         return dp[m][n] = solve(s, t, m - 1, n);
+    //     }
 
-        // we need to return something for the function(DUMMY STATEMENT/ NOT REACHABLE)
-        return -1;
-    }
+    //     // we need to return something for the function(DUMMY STATEMENT/ NOT REACHABLE)
+    //     return -1;
+    // }
 
     int numDistinct(string s, string t) {
+        
+        // TABULATION
         int m = s.size();
         int n = t.size();
 
-        memset(dp, -1, sizeof(dp));
+        vector<vector<ull>> dp(m + 1, vector<ull>(n + 1));
 
-        return solve(s, t, m, n);
+        for(int i = 0; i <= m; i++) {
+            dp[i][0] = 1;
+        }
+        for(int i = 1; i <= n; i++) {
+            dp[0][i] = 0;
+        }
+
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(s[i - 1] == t[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[m][n];
+        
     }
 };
