d09cb3d15241b9cbed1e712340296c1a3e008352
Krtk22 <kushwahakartik43@gmail.com>
2026-09-09 09:54:02 +0530

    Time: 16 ms (78.77%), Space: 41.7 MB (88.33%) - LeetHub

--- 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.cpp
+++ 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.cpp
@@ -1,22 +1,32 @@
 class Solution {
+    // OPTIMIZATION(Sliding window + hashmap)
 public:
     int countCompleteSubarrays(vector<int>& nums) {
 
         int n = nums.size();
 
         // Taking set for distinct elements
-        set<int> distinctEle(nums.begin(), nums.end());
+        unordered_set<int> distinctElement(nums.begin(), nums.end());
 
-        int distEle = distinctEle.size();
+        int totalDistinctElement = distinctElement.size();
 
 
-        // Double loop
+        unordered_map<int, int> freq;
         int count = 0;
-        for(int i = 0; i < n; i++) {
-            set<int> temp;
-            for(int j = i; j < n; j++) {
-                temp.insert(nums[j]);
-                if(temp.size() == distEle) count++;
+        int left = 0;
+
+        for(int right = 0; right < n; right++) {
+            freq[nums[right]]++;
+
+            // Shrink window until it has all distinct elements
+            while(freq.size() == totalDistinctElement) {
+                // Every subarray starting at left and ending at right…n-1 is valid
+                count += n - right;
+
+                // Remove nums[left] and move left forward
+                freq[nums[left]]--;
+                if(freq[nums[left]] == 0) freq.erase(nums[left]);
+                    left++;
             }
         }
 
