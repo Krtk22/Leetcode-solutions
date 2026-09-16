71d3bf86ff54c6a98e54e02ebc5612d567f12637
Krtk22 <kushwahakartik43@gmail.com>
2026-09-16 10:16:36 +0530

    Time: 2 ms (80.87%), Space: 9 MB (91.33%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -1,26 +1,33 @@
 class Solution {
 public:
     int maxPalindromes(string s, int k) {
-        int n = s.size();
-        if (k == 1) return n;
-
-        int res = 0;
-
-        for (int i = 0; i <= n - k;) {
-            int step = 1;
-
-            for (int d : {k, k + 1}) {
-                if (i + d <= n && equal(s.begin() + i, s.begin() + i + (d >> 1),
-                                        s.rbegin() + (n - (i + d)))) {
-                    step = d;
-                    res++;
-                    break;
-                }
-            }
-
-            i += step;
+        string st="*";
+        for(int i=0; i<s.size(); i++){
+            st+=s[i];
+            st+="*";
         }
-
-        return res;
+        int cnt=0;
+        int ans=0;
+        int j=0;
+        for(int i=k; i<st.size(); i++){
+            int l=i-1;
+            int r=i+1;
+            cnt=(st[i]=='*')?0:1;
+            while(l>=j && r<st.size() && cnt<k){
+                if(st[l]==st[r]){
+                    if(st[l]!='*')cnt+=2;
+                    l--;
+                    r++;
+                }
+                else break;
+            }
+            // cout<<cnt<<"-"<<st[i]<<endl;
+            if(cnt>=k){
+                ans++;
+                i=r-1;
+                j=r;
+            }
+        }
+        return ans;
     }
 };
